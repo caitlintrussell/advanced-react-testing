a99775440dd19aa3ac471dd3260661c2417f69c2
Caitlin Trussell <caitlintrussell@gmail.com>
2018-06-20 19:27:31 -0500

    comment box in progress

--- src/components/comment-box.js
+++ src/components/comment-box.js
@@ -9,14 +9,13 @@ class CommentBox extends Component {
   }
   handleSubmit = (event) => {
     event.preventDefault();
-
     this.setState({comment: ''});
   }
   render () {
     return (
       <form onSubmit={this.handleSubmit}>
         <h4>Comment</h4>
-        <textArea value={this.state.comment} onChange={this.handleChange} />
+        <textarea value={this.state.comment} onChange={this.handleChange} />
         <div>
           <button>Submit Comment</button>
         </div>
