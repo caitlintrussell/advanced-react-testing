4e0ee1a8488dd22fc2233f14afd526d3019e0c77
Caitlin Trussell <caitlintrussell@gmail.com>
2018-07-25 17:53:53 -0500

    added a test

--- src/components/__tests__/comment-box.test.js
+++ src/components/__tests__/comment-box.test.js
@@ -21,7 +21,16 @@ it('has a text area that users can type in', () => {
     target: { value: 'new comment'}
   });
   wrapped.update();
-
   expect(wrapped.find('textarea').prop('value')).toEqual('new comment');
+});
 
+it('text area is cleared on submit', () => {
+  wrapped.find('textarea').simulate('change', {
+    target: { value: 'look at me'}
+  });
+  wrapped.update();
+  expect(wrapped.find('textarea').prop('value')).toEqual('look at me');
+  wrapped.find('form').simulate('submit');
+  wrapped.update();
+  expect(wrapped.find('textarea').prop('value')).toEqual('');
 });
